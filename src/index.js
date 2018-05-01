a83fe05982e0b70643dd2e5ffe8ad55a35d4a948
Ross Evans <therealrossevans@gmail.com>
2018-04-30 23:28:50 -0500

    Add eslint + prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import openSocket from 'socket.io-client';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8080');
+
+openSocket('http://localhost:8080');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
