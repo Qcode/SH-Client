e947ee1dd5254cc7cbfd2ff6679c7d050ce48766
Ross Evans <therealrossevans@gmail.com>
2018-05-01 16:36:10 -0500

    Remove package.lock

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import openSocket from 'socket.io-client';
+import io from 'socket.io-client';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-openSocket('http://localhost:8080');
+const socket = io('http://localhost:8080');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
