1ed5e300b22cd9489d4bae9e6ba7d03199731a80
Ross Evans <therealrossevans@gmail.com>
2018-05-01 21:44:03 -0500

    Move display into GameContainer

---
+++ src/GameContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GameContainer() {
+  return (
+    <div className="game-container">
+      <h1>Lobby</h1>
+    </div>
+  );
+}
+
+export default GameContainer;
