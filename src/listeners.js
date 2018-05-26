ca28ad1c32eb7b3eb439ad12307515371dc8b84d
Ross Evans <therealrossevans@gmail.com>
2018-05-25 21:29:25 -0400

    Standardize event syntax

--- src/listeners.js
+++ src/listeners.js
@@ -2,32 +2,32 @@ import { socket, store } from './objects';
 import {
   SYNC_USERS,
   SYNC_USER,
-  CHANGE_GAME_STATE,
-  CHANGE_GAME_STAGE,
-  GET_SCORE,
+  SET_GAME_STATE,
+  SET_GAME_STAGE,
+  SYNC_SCORE,
   GAME_OVER_REASON,
 } from './reducers/eventTypes';
 
-socket.on('users', (data) => {
+socket.on(SYNC_USERS, (data) => {
   store.dispatch({ type: SYNC_USERS, users: data });
 });
 
-socket.on('user', (data) => {
+socket.on(SYNC_USER, (data) => {
   store.dispatch({ type: SYNC_USER, user: data });
 });
 
-socket.on('CHANGE_GAME_STATE', (data) => {
-  store.dispatch({ type: CHANGE_GAME_STATE, newState: data });
+socket.on(SET_GAME_STATE, (data) => {
+  store.dispatch({ type: SET_GAME_STATE, newState: data });
 });
 
 socket.on(GAME_OVER_REASON, (data) => {
   store.dispatch({ type: GAME_OVER_REASON, reason: data });
 });
 
-socket.on('gameStage', (data) => {
-  store.dispatch({ type: CHANGE_GAME_STAGE, newStage: data });
+socket.on(SET_GAME_STAGE, (data) => {
+  store.dispatch({ type: SET_GAME_STAGE, newStage: data });
 });
 
-socket.on('score', (data) => {
-  store.dispatch({ type: GET_SCORE, score: data });
+socket.on(SYNC_SCORE, (data) => {
+  store.dispatch({ type: SYNC_SCORE, score: data });
 });
